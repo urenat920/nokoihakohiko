8b91ad0ad1c03af916dcbd0d6c77604cb3634f5f
yamahira <natureeng2021@gmail.com>
2023-01-22 14:39:46 +0900

    初期画面のルーディング設定完了

---
+++ src/app/Http/Controllers/IndexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //一旦初期画面を設定する
+        return view('index');
+    }
+}
